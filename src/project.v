3a45823ff1da75e624c4eeffa806488fc6725831
Brian Sierawski <sierawbd@users.noreply.github.com>
2025-10-18 16:15:12 -0500

    Add files via upload

--- src/project.v
+++ src/project.v
@@ -17,11 +17,11 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = not ui_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uo_out[0] = ~ui_in[0];
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, clk, rst_n, 1'b0, ui_in[7:1]};
 
 endmodule
