fd73ce2f32c219cb52889fc6a4d48d80a29216f5
Brian Sierawski <sierawbd@users.noreply.github.com>
2025-10-19 09:30:28 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -13,16 +13,20 @@ module tt_um_example (
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  wire       rst_n,    // reset_n - low to reset
+    wire w001
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out[7:1] = 1;
-  assign uo_out[0] = ~ui_in[0];
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    // All output pins must be assigned. If not used, assign to 0.
+    assign uo_out[7:1] = 1;
+    //assign uo_out[0] = ~ui_in[0];
+    assign uio_out = 0;
+    assign uio_oe  = 0;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0, uio_in[7:0], ui_in[7:1]};
+    assign w001 = ~ui_in[0];
+    assign uo_out[0] = ~w0001;    
+    
+    // List all unused inputs to prevent warnings
+    wire _unused = &{ena, clk, rst_n, 1'b0, uio_in[7:0], ui_in[7:1]};
 
 endmodule
